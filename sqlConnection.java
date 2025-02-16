ab1b3b107b0f0df92f3b96f8a301d87b7f0942f0
D00258621 <d00258621@student.dkit.ie>
2025-02-16 08:06:30 +0000

    Create expenses.java (DTO)

--- sqlConnection.java
+++ sqlConnection.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class sqlConnection {
-    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String URL = "jdbc:mysql://localhost:3306/finance_tracker";
     public static final String USER = "root";
     public static final String PASSWORD = "";
 
